5020bf775cf3316abfe345f0859d5c0de73b4194
Oliver Breitwieser <oliver.breitwieser@gmail.com>
2009-09-21 20:48:08 +0200

    Map angelegt
    Signed-off-by: Oliver Breitwieser <oliver.breitwieser@gmail.com>

---
+++ Map.cpp
@@ -0,0 +1,10 @@
+/*
+ *  Map.h
+ *  Prototyp
+ *
+ *  Created by Oliver Breitwieser on 07.09.09.
+ *  Copyright 2009 Universität Heidelberg. All rights reserved.
+ *
+ */
+ 
+ #include "Map.h"
---
+++ Map.h
@@ -0,0 +1,33 @@
+/*
+ *  Map.h
+ *  Prototyp
+ *
+ *  Created by Oliver Breitwieser on 07.09.09.
+ *  Copyright 2009 Universität Heidelberg. All rights reserved.
+ *
+ */
+ 
+#ifndef MAP_H
+#define MAP_H
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+#include <Carbon/Carbon.h>
+#endif
+
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+namespace Orbifold {
+
+class Map {
+private:
+	// Chart* chart; // not implemented yet
+	Ogre::SceneManager* scenemanager;
+
+protected:
+
+public:
+
+}
+
+#endif
